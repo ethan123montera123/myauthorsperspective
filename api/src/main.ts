8e79869d40a3f048fc5d239dbe8ff16822b3976a
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-06-10 01:01:32 +0800

    [Config][Lex] Fixed implementation of api versioning and added compression package

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,17 @@
+import { VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import compression from "compression";
 import helmet from "helmet";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api/v1");
+  app.setGlobalPrefix("api");
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: "1",
+  });
 
   const config = new DocumentBuilder()
     .setTitle("My Author's Perspective API")
@@ -18,6 +24,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("docs", app, document);
 
+  app.use(compression());
   app.use(helmet());
   app.enableCors({
     origin: "*",

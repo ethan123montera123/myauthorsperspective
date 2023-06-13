163071b17a550aceec13a912eebd4290baf14d0b
Luzefiru <ian.deejay.iv@gmail.com>
2023-06-13 18:42:14 +0800

    [Ian][Feat] finish Services layout

--- client/components/layout-services.jsx
+++ client/components/layout-services.jsx
@@ -1,9 +1,54 @@
+// icons from https://lucide.dev/guide/packages/lucide-react
+import { ChevronsUp } from "lucide-react";
+
+import Image from "next/image";
+import Link from "next/link";
+
 export default function LayoutServices({ children }) {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
-      <header>Hello World</header>
-      {children}
-      <footer>Footer</footer>
+      <header className="w-full flex justify-between items-center pl-8 pr-6 py-1 shadow-[0_0_12px_rgba(0,0,0,0.33)]">
+        <Link href="/" aria-label="Homepage">
+          <Image
+            src="/images/logo.png"
+            alt="My Author's Perspective"
+            width="80"
+            height="80"
+          />
+        </Link>
+        <div className="flex gap-4 font-medium">
+          <Link
+            href="/services"
+            className="uppercase hover:font-bold py-4 px-2"
+          >
+            Services
+          </Link>
+          <Link href="/about" className="uppercase hover:font-bold py-4 px-2">
+            About
+          </Link>
+          <Link href="/contact" className="uppercase hover:font-bold py-4 px-2">
+            Contact
+          </Link>
+          <Link href="/cart" className="uppercase hover:font-bold py-4 px-2">
+            Cart (0)
+          </Link>
+        </div>
+      </header>
+      <main>{children}</main>
+      <footer className="mt-auto bg-neutral-700 text-white flex justify-center py-2 uppercase">
+        <div
+          aria-label="Scroll To Top"
+          onClick={scrollToTop}
+          className="-translate-y-[calc(100%+6px)] bg-neutral-700 absolute px-3 pt-1 rounded-t-3xl hover:cursor-pointer"
+        >
+          <ChevronsUp color="white" />
+        </div>
+        ©2023 | My Author&rsquo;s Perspective
+      </footer>
     </>
   );
 }
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,17 +1,27 @@
 import "@/styles/globals.css";
 import LayoutServices from "@/components/layout-services";
 
+// fonts
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps, router }) {
   const path = router.pathname;
 
   if (path.startsWith("/services")) {
     return (
-      <LayoutServices>
-        <Component {...pageProps} />
-      </LayoutServices>
+      <div className={`flex flex-col h-full ${inter.className}`}>
+        <LayoutServices font={inter.className}>
+          <Component {...pageProps} />
+        </LayoutServices>
+      </div>
     );
   } else {
-    return <Component {...pageProps} />;
+    return (
+      <div className={inter.className}>
+        <Component {...pageProps} />
+      </div>
+    );
   }
 }
 

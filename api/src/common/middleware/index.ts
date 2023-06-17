aba0596a3d3e08c4eea26258674edcb4a0fd09aa
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-06-17 14:05:02 +0800

    [Misc][Lex] Removed API folder

--- api/src/common/middleware/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as RequestLoggerMiddleware } from "./request-logger.middleware";
--- api/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-import cors from "./cors";
-import swagger from "./swagger";
-
-export * from "./app";
-export * from "./cors";
-export * from "./swagger";
-
-export default () => Object.freeze({ app, cors, swagger });

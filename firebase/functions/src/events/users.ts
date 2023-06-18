b0d4cd36df60674d27ed114cc76017b4e70d1a1f
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-06-18 19:06:44 +0800

    [Feat][Lex] Added updates syncing between stripe account and firebase collections

--- firebase/functions/src/events/users.ts
+++ firebase/functions/src/events/users.ts
@@ -1,4 +1,7 @@
-import { onDocumentCreated } from "firebase-functions/v2/firestore";
+import {
+  onDocumentCreated,
+  onDocumentUpdated,
+} from "firebase-functions/v2/firestore";
 import { config, logger, stripe } from "../providers";
 
 const { users } = config.firebase.collectionPaths;
@@ -35,6 +38,41 @@ export const createStripeAccount = onDocumentCreated(
       logger.error("Stripe account creation failed.", error as Error, {
         user: uid,
       });
+    }
+
+    return null;
+  }
+);
+
+export const syncAccountUpdateToStripe = onDocumentUpdated(
+  `${users}/{uid}`,
+  async function ({ data: snapshot, params: { uid } }) {
+    if (!snapshot) {
+      logger.warn("Snapshot did not contain any data.", { user: uid });
+      return null;
+    }
+
+    const { stripeId, ...data } = snapshot.after.data();
+    const account = { user: uid, stripe: stripeId };
+
+    try {
+      logger.log("Syncing updates to stripe account...", account);
+
+      await stripe.customers.update(stripeId, {
+        name: data.firstName + " " + data.lastName,
+        email: data.email,
+        phone: data.phone,
+      });
+
+      logger.log("Stripe account successfully synced with updates.", account);
+
+      return snapshot.after.data();
+    } catch (error: unknown) {
+      logger.error(
+        "Stripe account update syncing failed.",
+        error as Error,
+        account
+      );
     }
 
     return null;

06aabc13a8cbc381ba516c42ffefe927dd73655c
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-06-20 18:41:19 +0800

    [Feat][Lex] Added change password method

---
+++ client/services/auth/password.js
@@ -0,0 +1,30 @@
+import { verifyAuthLogon } from "@/services/common";
+import { auth } from "@/services/firebase";
+import { updatePassword } from "firebase/auth";
+
+/**
+ * Change the password of the currently authenticated user.
+ *
+ * @param {string} password - The new password to change to.
+ * @returns {Promise<void>}
+ * @remarks
+ * Note: This is a sensitive operation, thus it is necessary to reauthenticate the user
+ * credentials before using this method.
+ *
+ * @example
+ * ```js
+ * try {
+ *  // This is necessary for security
+ *  await reauthenticateWithCredentials(password);
+ *  await changePassword(newPassword);
+ *  // handle data
+ * } catch (error) {
+ *  // handle error
+ * }
+ * ```
+ */
+export async function changePassword(password) {
+  verifyAuthLogon(auth);
+
+  await updatePassword(auth.currentUser, password);
+}

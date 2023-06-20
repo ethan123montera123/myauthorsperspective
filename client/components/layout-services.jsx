c893901c27c71348b2b525e23bc9f502c026a3d7
Luzefiru <ian.deejay.iv@gmail.com>
2023-06-20 16:59:14 +0800

    [Feat][Ian] make navbar responsive for mobile screens

--- client/components/layout-services.jsx
+++ client/components/layout-services.jsx
@@ -1,12 +1,19 @@
 // icons from https://lucide.dev/guide/packages/lucide-react
-import { ChevronsUp } from "lucide-react";
+import { ChevronsUp, Menu, X } from "lucide-react";
 
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function LayoutServices({ children, router }) {
   const CURRENT_PATH = useRouter().pathname;
+
+  const [isShowingLinks, setIsShowingLinks] = useState(false);
+
+  const toggleShowLinks = () => {
+    setIsShowingLinks(!isShowingLinks);
+  };
 
   const isSelected = (category) => {
     // if the category name is found in the URL path
@@ -21,9 +28,52 @@ export default function LayoutServices({ children, router }) {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  const generateMainContainer = () => {
+    if (isShowingLinks === false) {
+      return <main className="mx-8 my-6 lg:mx-32 lg:my-8">{children}</main>;
+    } else {
+      // returns a customized view of the links for mobile devices
+      return (
+        <>
+          <main className="mx-8 my-2 lg:mx-32 lg:my-8">{children}</main>
+          <div className="absolute w-full top-[88px] pb-6 flex flex-col bg-white z-10 items-center text-xl shadow-xl">
+            <Link
+              href="/services"
+              className="uppercase py-4 px-2 relative"
+              id={isSelected("services")}
+            >
+              Services
+            </Link>
+            <Link
+              href="/about"
+              className="uppercase py-4 px-2"
+              id={isSelected("about")}
+            >
+              About
+            </Link>
+            <Link
+              href="/contact"
+              className="uppercase py-4 px-2"
+              id={isSelected("contact")}
+            >
+              Contact
+            </Link>
+            <Link
+              href="/cart"
+              className="uppercase py-4 px-2"
+              id={isSelected("cart")}
+            >
+              Cart (0)
+            </Link>
+          </div>
+        </>
+      );
+    }
+  };
+
   return (
     <>
-      <header className="w-full flex justify-between items-center pl-8 pr-6 py-1 shadow-[0_0_12px_rgba(0,0,0,0.33)]">
+      <header className="relative w-full flex justify-between flex-row items-center pl-4 pr-6 lg:pl-8 lg:pr-6 py-1 shadow-[0_0_12px_rgba(0,0,0,0.33)]">
         <Link href="/" aria-label="Homepage">
           <Image
             src="/images/logo.png"
@@ -32,7 +82,14 @@ export default function LayoutServices({ children, router }) {
             height="80"
           />
         </Link>
-        <div className="flex gap-4 font-medium">
+        <button className="lg:hidden" onClick={toggleShowLinks}>
+          {isShowingLinks ? (
+            <X color="black" size="40px" />
+          ) : (
+            <Menu color="black" size="40px" />
+          )}
+        </button>
+        <div className={`hidden lg:flex gap-4 font-medium`}>
           <Link
             href="/services"
             className="uppercase py-4 px-2 relative"
@@ -63,7 +120,7 @@ export default function LayoutServices({ children, router }) {
           </Link>
         </div>
       </header>
-      <main className="mx-32 my-8">{children}</main>
+      {generateMainContainer()}
       <footer className="text-xs md:text-base mt-auto bg-neutral-700 text-white flex justify-center py-2 uppercase">
         <div
           aria-label="Scroll To Top"

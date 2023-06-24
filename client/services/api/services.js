92c923698f19b0abb55aa64e05ebc92438928a12
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-06-25 01:13:19 +0800

    [Feat][Lex] Created services API

---
+++ client/services/api/services.js
@@ -0,0 +1,27 @@
+import { collection, getDocs } from "firebase/firestore";
+import { collections, db } from "../firebase";
+
+/**
+ * Gets the offered services from the database.
+ *
+ * @returns {Promise<import("@/services/common/@types").Service[]>} The services from the database.
+ */
+export async function getServices() {
+  const servicesRef = collection(db, collections.SERVICES);
+  const snapshot = await getDocs(servicesRef);
+
+  /** @type {import("@/services/common/@types").Service[]} */
+  const docs = [];
+  snapshot.forEach((doc) => {
+    const { inclusions, title, unitPrice } = doc.data();
+
+    docs.push({
+      id: doc.id,
+      title,
+      unitPrice,
+      inclusions: inclusions.map((name, id) => ({ id, name })),
+    });
+  });
+
+  return docs;
+}

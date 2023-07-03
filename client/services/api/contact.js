1233cde3721cd5a142cf1787e61d5e069208da04
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-07-03 17:26:07 +0800

    [Feat][Lex] Created Contact api for client

---
+++ client/services/api/contact.js
@@ -0,0 +1,29 @@
+import { httpsCallable } from "firebase/functions";
+
+import { callables, functions } from "@/services/firebase";
+import { ObjectWithError, parseThrowablesToObject } from "@/services/utils";
+
+/**
+ * Contact gateway for the customers to send an email to the company.
+ *
+ * @param   {import("./@types").ContactData[]} contact - The data associated with the contact.
+ * @returns {Promise<ObjectWithError<void>>} An object containing a possible error from the execution.
+ *
+ * @example
+ * const contact = {
+ *  firstName: "John",
+ *  lastName: "Smith",
+ *  email: "john@smith.com",
+ *  subject: "Test Subject",
+ *  message: "Lorem Ipsum",
+ * };
+ *
+ * const { data } = await sendContactEmail(contact);
+ * if(error) // handle error
+ */
+export async function sendContactEmail(contact) {
+  return parseThrowablesToObject(async () => {
+    const fn = httpsCallable(functions, callables.sendContactEmail);
+    await fn(contact);
+  });
+}
--- client/services/api/index.js
+++ client/services/api/index.js
@@ -1,3 +1,4 @@
 export * as auth from "./auth";
+export * as contact from "./contact";
 export * as services from "./services";
 export * as transaction from "./transaction";

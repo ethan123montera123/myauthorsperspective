fc957718439e0fa32b8e3598a426ccf6c6c8dcda
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-07-06 04:56:54 +0800

    [Fix][Lex] Fixed globalization of app check when it should be unique per page request

--- client/services/firebase/appCheck.js
+++ client/services/firebase/appCheck.js
@@ -1,15 +1,7 @@
 import { ReCaptchaV3Provider, initializeAppCheck } from "firebase/app-check";
-import { createRef, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { app } from "./firebase";
-
-/**
- * @type {import("react").RefObject<import("firebase/app-check").AppCheck | null>}
- *
- * This is a global variable that references the app check instance, to ensure
- * that only one copy is created per app.
- */
-const appCheck = createRef(null);
 
 /**
  * Hook for initializing app check.
@@ -21,9 +13,11 @@ const appCheck = createRef(null);
  * You must run this at `_app.js` to ensure that it always run at app start-up, and only
  * runs once.
  *
- * @returns {void} Does not return anything, as it just initializes app check.
+ * @returns {import("firebase/app-check").AppCheck | null} Returns the app check instance.
  */
 export function useAppCheck() {
+  const [appCheck, setAppCheck] = useState(null);
+
   useEffect(() => {
     const SITE_KEY = process.env.NEXT_PUBLIC_FIREBASE_RECAPTCHA_SITE_KEY;
     const DEBUG_TOKEN = process.env.NEXT_PUBLIC_FIREBASE_APPCHECK_DEBUG_TOKEN;
@@ -31,17 +25,19 @@ export function useAppCheck() {
 
     if (!SITE_KEY || (NODE_ENV !== "production" && !DEBUG_TOKEN)) return;
 
-    if (appCheck.current === null) {
+    if (appCheck === null) {
       if (NODE_ENV !== "production" && DEBUG_TOKEN) {
         // This is to mock the token, and not create unnecessary logs to ReCAPTCHA
         // during development
         Object.assign(window, { FIREBASE_APPCHECK_DEBUG_TOKEN: DEBUG_TOKEN });
       }
 
-      appCheck.current = initializeAppCheck(app, {
+      const appCheckInstance = initializeAppCheck(app, {
         provider: new ReCaptchaV3Provider(SITE_KEY),
         isTokenAutoRefreshEnabled: true,
       });
+
+      setAppCheck(appCheckInstance);
     }
 
     return () => {
@@ -49,4 +45,6 @@ export function useAppCheck() {
       delete window.FIREBASE_APPCHECK_DEBUG_TOKEN;
     };
   }, []);
+
+  return appCheck;
 }

ba0a10d7a4fc7f9fcfd9c5cc4b97f3976745bd77
Luzefiru <ian.deejay.iv@gmail.com>
2023-07-08 14:57:45 +0800

    [Feat][Ian] add proper metadata for page titles

--- client/pages/404.js
+++ client/pages/404.js
@@ -1,12 +1,22 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Custom404() {
   return (
-    <div className="mt-[15lvh] xl:mt-[25lvh] grid place-items-center gap-4">
-      <div>Error 404: Page Not Found</div>
-      <Link href="/" className="text-[#00C1EB] hover:underline">
-        Back to home?
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Page Not Found</title>
+        <meta
+          name="description"
+          content="Page not found, please go back to the Home page to continue browsing."
+        />
+      </Head>
+      <div className="mt-[15lvh] xl:mt-[25lvh] grid place-items-center gap-4">
+        <div>Error 404: Page Not Found</div>
+        <Link href="/" className="text-[#00C1EB] hover:underline">
+          Back to home?
+        </Link>
+      </div>
+    </>
   );
 }

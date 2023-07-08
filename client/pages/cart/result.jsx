22eb7ec099d847c07a8ee8f3da7f70b51a4f3f15
Luzefiru <ian.deejay.iv@gmail.com>
2023-07-08 13:05:06 +0800

    [Feat][Ian] add loading spinner to checkout page

--- client/pages/cart/result.jsx
+++ client/pages/cart/result.jsx
@@ -1,7 +1,10 @@
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function CartResult() {
+  const [isLoading, setIsLoading] = useState(true);
   const searchParams = useSearchParams();
 
   const paymentWasSuccessful =
@@ -10,6 +13,23 @@ export default function CartResult() {
   const message = paymentWasSuccessful
     ? "Your payment was successful."
     : "Your payment failed, please try again or contact the developers.";
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 3000);
+
+  if (isLoading) {
+    return (
+      <div className="mt-[15lvh] xl:mt-[25lvh] grid place-items-center gap-4">
+        <Image
+          src="/images/loading.svg"
+          alt="Loading"
+          width="128"
+          height="128"
+        />
+      </div>
+    );
+  }
 
   return (
     <div className="mt-[15lvh] xl:mt-[25lvh] grid place-items-center gap-4">
